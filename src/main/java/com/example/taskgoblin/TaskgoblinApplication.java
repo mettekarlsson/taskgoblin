5ad94a33731d5473d5bd7c23b1bc2b2ef4f8384f
Mette Karlsson <mettek_96@hotmail.com>
2026-05-12 11:12:02 +0200

    Initial commit

---
+++ src/main/java/com/example/taskgoblin/TaskgoblinApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskgoblin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskgoblinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskgoblinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taskgoblin/TaskgoblinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskgoblin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskgoblinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
