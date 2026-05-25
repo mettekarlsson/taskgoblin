42cd0dd321a44cef1dd3f34b35898a0bdc00f01c
SofieTornqvist <sofietornqvist@hotmail.com>
2026-05-25 09:55:01 +0200

    Create UserSettingsDTO

---
+++ src/main/java/com/example/taskgoblin/dto/UserSettingsDTO.java
@@ -0,0 +1,68 @@
+package com.example.taskgoblin.dto;
+
+import com.example.taskgoblin.model.Language;
+import com.example.taskgoblin.model.User;
+
+public class UserSettingsDTO {
+
+    private User user;
+
+    private Integer defaultReminderMinutes;
+
+    private Boolean notificationsEnabled;
+
+    private String theme;
+
+    private Language language;
+
+    public UserSettingsDTO(User user, Integer defaultReminderMinutes, Boolean notificationsEnabled, String theme, Language language) {
+        this.user = user;
+        this.defaultReminderMinutes = defaultReminderMinutes;
+        this.notificationsEnabled = notificationsEnabled;
+        this.theme = theme;
+        this.language = language;
+
+
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Integer getDefaultReminderMinutes() {
+        return defaultReminderMinutes;
+    }
+
+    public void setDefaultReminderMinutes(Integer defaultReminderMinutes) {
+        this.defaultReminderMinutes = defaultReminderMinutes;
+    }
+
+    public Boolean getNotificationsEnabled() {
+        return notificationsEnabled;
+    }
+
+    public void setNotificationsEnabled(Boolean notificationsEnabled) {
+        this.notificationsEnabled = notificationsEnabled;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+}
+
