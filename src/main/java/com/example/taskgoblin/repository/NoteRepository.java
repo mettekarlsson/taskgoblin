6281c1d1d5c73f72d4ee399f33a3ab22eb66af1c
Caroline Enggren <caroline.enggren@gmail.com>
2026-05-25 12:38:17 +0200

    create NoteRepository.java

---
+++ src/main/java/com/example/taskgoblin/repository/NoteRepository.java
@@ -0,0 +1,11 @@
+package com.example.taskgoblin.repository;
+
+import com.example.taskgoblin.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
+
