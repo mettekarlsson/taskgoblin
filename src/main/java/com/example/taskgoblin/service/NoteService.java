ce40f5923a019b3739dd87066b13eb7070977807
Caroline Enggren <caroline.enggren@gmail.com>
2026-05-25 13:44:04 +0200

    create endpoints for /notes and /notes/{id}.

--- src/main/java/com/example/taskgoblin/service/NoteService.java
+++ src/main/java/com/example/taskgoblin/service/NoteService.java
@@ -17,16 +17,16 @@ public class NoteService {
         this.noteRepository = noteRepository;
     }
 
-    public List<NoteDTO> getAllNotes() {
-        List<Note> notes = noteRepository.findAll();
+    public List<NoteDTO> getAllNotes(Long id) {
+        List<Note> notes = noteRepository.findByUserId(id);
 
         return notes.stream()
                 .map(NoteMapper::mapToNoteDto)
                 .toList();
     }
 
-    public NoteDTO getNote(Long id) {
-        Note note = noteRepository.findById(id)
+    public NoteDTO getNote(Long id, Long userId) {
+        Note note = noteRepository.findByIdAndUserId(id, userId)
                 .orElseThrow(() -> new RuntimeException("Note not found"));
 
         return NoteMapper.mapToNoteDto(note);
