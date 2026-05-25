4e488bef591750a3db25be4733148b3796ef27ab
SofieTornqvist <sofietornqvist@hotmail.com>
2026-05-25 10:08:19 +0200

    Add UserSettingsService and UserSettingsRepository

---
+++ src/main/java/com/example/taskgoblin/repository/UserSettingsRepository.java
@@ -0,0 +1,9 @@
+package com.example.taskgoblin.repository;
+
+import com.example.taskgoblin.model.UserSettings;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserSettingsRepository extends JpaRepository<UserSettings, Long> {
+}
---
+++ src/main/java/com/example/taskgoblin/service/UserSettingsService.java
@@ -0,0 +1,16 @@
+package com.example.taskgoblin.service;
+
+import com.example.taskgoblin.repository.UserSettingsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserSettingsService {
+
+    private final UserSettingsRepository userSettingsRepository;
+
+    public UserSettingsService(UserSettingsRepository userSettingsRepository) {
+        this.userSettingsRepository = userSettingsRepository;
+    }
+
+
+}
