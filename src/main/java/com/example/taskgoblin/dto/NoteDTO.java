085593bf44757e9087cb965da3952d1680bc04fe
Caroline Enggren <caroline.enggren@gmail.com>
2026-05-25 12:32:31 +0200

    create NoteDTO.java

---
+++ src/main/java/com/example/taskgoblin/dto/NoteDTO.java
@@ -0,0 +1,79 @@
+package com.example.taskgoblin.dto;
+
+import jakarta.persistence.Column;
+
+import java.time.LocalDateTime;
+
+public class NoteDTO {
+
+    private String title;
+
+    private String content;
+
+    private String color;
+
+    private Boolean pinned;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime lastInteractedAt;
+
+    public NoteDTO(String title, String content, String color, Boolean pinned, LocalDateTime createdAt, LocalDateTime lastInteractedAt) {
+        this.title = title;
+        this.content = content;
+        this.color = color;
+        this.pinned = pinned;
+        this.createdAt = createdAt;
+        this.lastInteractedAt = lastInteractedAt;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Boolean getPinned() {
+        return pinned;
+    }
+
+    public void setPinned(Boolean pinned) {
+        this.pinned = pinned;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getLastInteractedAt() {
+        return lastInteractedAt;
+    }
+
+    public void setLastInteractedAt(LocalDateTime lastInteractedAt) {
+        this.lastInteractedAt = lastInteractedAt;
+    }
+}
+
+
