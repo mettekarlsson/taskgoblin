c0cb73fde73040b198405dfc109ee19e17e261b5
SofieTornqvist <sofietornqvist@hotmail.com>
2026-05-28 09:38:38 +0200

    Add method getListById in TaskListService

--- src/main/java/com/example/taskgoblin/service/TaskListService.java
+++ src/main/java/com/example/taskgoblin/service/TaskListService.java
@@ -1,6 +1,7 @@
 package com.example.taskgoblin.service;
 
 import com.example.taskgoblin.dto.TaskListDTO;
+import com.example.taskgoblin.exception.ResourceNotFoundException;
 import com.example.taskgoblin.mapper.TaskListMapper;
 import com.example.taskgoblin.model.TaskList;
 import com.example.taskgoblin.repository.TaskListRepository;
@@ -17,6 +18,7 @@ public class TaskListService {
         this.taskListRepository = taskListRepository;
     }
 
+
     public List<TaskListDTO> getAllListsForUser(Long userId) {
 
         List<TaskList> taskLists = taskListRepository.findByUserId(userId);
@@ -24,5 +26,15 @@ public class TaskListService {
         return taskLists.stream()
                 .map(TaskListMapper::mapToTaskListDTO)
                 .toList();
+    }
+
+    public TaskListDTO getListById(Long listId, Long userId) {
+
+        TaskList taskList = taskListRepository
+                .findByIdAndUserId(listId, userId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Task list not found"));
+
+        return TaskListMapper.mapToTaskListDTO(taskList);
     }
 }
