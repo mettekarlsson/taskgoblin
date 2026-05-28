93f100388d6a817d844909ddc9726c580a9a021c
SofieTornqvist <sofietornqvist@hotmail.com>
2026-05-28 09:35:37 +0200

    Add method findByIdAndUserId to TaskListRepository

--- src/main/java/com/example/taskgoblin/repository/TaskListRepository.java
+++ src/main/java/com/example/taskgoblin/repository/TaskListRepository.java
@@ -4,9 +4,12 @@ import com.example.taskgoblin.model.TaskList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskListRepository extends JpaRepository<TaskList, Long> {
 
     List<TaskList> findByUserId(Long userId);
+
+    Optional<TaskList> findByIdAndUserId(Long id, Long userId);
 
 }
