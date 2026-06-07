bf86be7fefe431d73177417d780a8f276874ada7
SofieTornqvist <sofietornqvist@hotmail.com>
2026-06-07 14:14:12 +0200

    Add methods createCategory in CategoryService and CategoryController

--- src/main/java/com/example/taskgoblin/controller/CategoryController.java
+++ src/main/java/com/example/taskgoblin/controller/CategoryController.java
@@ -1,4 +1,54 @@
 package com.example.taskgoblin.controller;
 
+import com.example.taskgoblin.dto.CategoryDTO;
+import com.example.taskgoblin.dto.CreateCategoryDTO;
+import com.example.taskgoblin.service.CategoryService;
+import com.example.taskgoblin.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categories")
 public class CategoryController {
+
+    /*
+     * Services
+     */
+
+    private final UserService userService;
+    private final CategoryService categoryService;
+
+    /*
+     * Constructor injection
+     */
+    public CategoryController(
+            UserService userService,
+            CategoryService categoryService
+    ) {
+
+        this.userService = userService;
+        this.categoryService = categoryService;
+    }
+
+    /*
+     * Create operations
+     */
+    @PostMapping
+    public ResponseEntity<CategoryDTO> createCategory(
+            @Valid @RequestBody CreateCategoryDTO createCategoryDTO,
+            @AuthenticationPrincipal UserDetails userDetails
+            ) {
+            Long userId = userService.getUserByEmail(userDetails.getUsername()).getId();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(categoryService.createCategory(userId, createCategoryDTO));
+            }
+
+
 }
--- src/main/java/com/example/taskgoblin/service/CategoryService.java
+++ src/main/java/com/example/taskgoblin/service/CategoryService.java
@@ -1,4 +1,50 @@
 package com.example.taskgoblin.service;
 
+import com.example.taskgoblin.dto.CategoryDTO;
+import com.example.taskgoblin.dto.CreateCategoryDTO;
+import com.example.taskgoblin.exception.ResourceNotFoundException;
+import com.example.taskgoblin.mapper.CategoryMapper;
+import com.example.taskgoblin.model.Category;
+import com.example.taskgoblin.model.User;
+import com.example.taskgoblin.repository.CategoryRepository;
+import com.example.taskgoblin.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class CategoryService {
+
+    /*
+     * Repositories
+     */
+    private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
+
+    /*
+     * Constructor injection
+     */
+    public CategoryService(
+            UserRepository userRepository,
+            CategoryRepository categoryRepository
+    ) {
+
+        this.userRepository = userRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    /*
+     * Create
+     */
+
+    // Creates and saves a new category for a specific user.
+    public CategoryDTO createCategory(Long userId, CreateCategoryDTO createCategoryDto) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User"));
+        Category category = CategoryMapper.mapToCategoryEntity(createCategoryDto);
+        category.setUser(user);
+
+        Category savedCategory = categoryRepository.save(category);
+        return CategoryMapper.mapToCategoryDto(savedCategory);
+    }
+
 }
