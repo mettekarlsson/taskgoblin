0e0fe28b2db258ad83bcd6faa253d53af7d4d3e5
Mette Karlsson <mettek_96@hotmail.com>
2026-08-18 12:20:17 +0200

    bug fixes

--- src/main/java/com/example/taskgoblin/controller/EventController.java
+++ src/main/java/com/example/taskgoblin/controller/EventController.java
@@ -2,7 +2,6 @@ package com.example.taskgoblin.controller;
 
 import com.example.taskgoblin.dto.CreateEventDTO;
 import com.example.taskgoblin.dto.EventDTO;
-import com.example.taskgoblin.dto.EventSummaryDTO;
 import com.example.taskgoblin.dto.UpdateEventDTO;
 import com.example.taskgoblin.service.CalendarService;
 import com.example.taskgoblin.service.UserService;
@@ -12,8 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping ("/events")
