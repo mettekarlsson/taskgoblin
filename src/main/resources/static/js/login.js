c6402a41541f7b145783cfb0f3b821a132af5a8e
Caroline Enggren <caroline.enggren@gmail.com>
2026-08-19 13:53:37 +0200

    Add login form submit handling

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,4 +1,6 @@
-const handleLogin = async () => {
+const handleLogin = async (event) => {
+
+    event.preventDefault();
 
     const email =
         document.getElementById("email").value;
@@ -23,6 +25,10 @@ const handleLogin = async () => {
     }
 
 };
+
+document
+    .getElementById("login-form")
+    .addEventListener("submit", handleLogin);
 
 const successMessage =
     document.getElementById("login-message");
